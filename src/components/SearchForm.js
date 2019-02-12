5198012a8ff4bb336f33578d14af021eca338d5d
rykyou <rkim1214@gmail.com>
2019-02-12 12:03:34 -0500

    finish primitive search Artwork forms

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -17,19 +17,19 @@ class SearchForm extends React.Component {
     })
   }
 
-  handleSearchSubmit = (e) => {
-    console.log("attempting to submit")
-    // this.props.addPokemon(this.state)
-    // e.target.reset()
+  handleSubmit = (e) => {
+    // console.log("attempting to submit")
+    this.props.handleSearchSubmit(this.state.searchTerm)
+    e.target.reset()
   }
 
   render() {
-    
+
     return (
       <div>
         <Grid>
           <Grid.Row centered>
-            <Form className="visit" onChange={this.handleSearchChange} onSubmit={this.handleSearchSubmit}>
+            <Form className="visit" onChange={this.handleSearchChange} onSubmit={this.handleSubmit}>
               <Form.Group widths="equal">
                 <Form.Input label="Filter by Title or Artist Name:" placeholder="Title or Artist Name" name="searchTerm" />
               </Form.Group>
--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -1,17 +1,45 @@
 import React, { Component } from 'react';
 import AllArtworksList from './AllArtworksList';
-import SearchForm from '../components/SearchForm';
+import { Form, Grid } from 'semantic-ui-react';
 
 class SearchContainer extends Component {
+  constructor() {
+    super()
+    this.state = {
+      searchTitle: '',
+      searchArtist: ''
+    }
+  }
+
+  handleSearchChange = (e) => {
+    // e.persist()
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
   render() {
+    let filteredArtworks = this.props.artworkArray.filter(artwork => artwork.title.toLowerCase().includes(this.state.searchTitle.toLowerCase()))
+    filteredArtworks = filteredArtworks.filter(artwork => artwork.artist_name.toLowerCase().includes(this.state.searchArtist.toLowerCase()))
+    
     return (
       <div>
         <h1>SearchContainer</h1>
-        <SearchForm />
+        <Grid>
+          <Grid.Row centered>
+            <Form className="visit" onChange={this.handleSearchChange} onSubmit={this.handleSubmit}>
+              <Form.Group widths="equal">
+                <Form.Input label="Filter by Title:" placeholder="Artist Name" name="searchTitle" />
+                <Form.Input label="Filter by Artist Name:" placeholder="Artist Name" name="searchArtist" />
+              </Form.Group>
+              <Form.Button>Search</Form.Button>
+            </Form>
+          </Grid.Row>
+        </Grid>
         <h3>All art:</h3>
         <div className="horizontal-scroll">
           <AllArtworksList
-            artworkArray={this.props.artworkArray}
+            artworkArray={filteredArtworks}
             handleArtworkClick={this.props.handleArtworkClick}
           />
         </div>
